b5ea1c43219f4ba444cea050ba36bdf9c0a1d4df
Si-Yual Park <siyual.bak@gmail.com>
2019-05-06 00:43:11 +0900

    .

--- src/tensor/Scalar.java
+++ src/tensor/Scalar.java
@@ -19,37 +19,19 @@ public interface Scalar extends Comparable<Scalar>, Cloneable {
     void add(Scalar rhs);
 
     static Scalar sub(Scalar lhs, Scalar rhs) {
-        try {
-            Scalar result = (Scalar)lhs.clone();
-            result.sub(rhs);
-            return result;
-        } catch (CloneNotSupportedException e) {
-            return null;
-        }
+        return null;
     }
 
     void sub(Scalar rhs);
 
     static Scalar mul(Scalar lhs, Scalar rhs) {
-        try {
-            Scalar result = (Scalar)lhs.clone();
-            result.mul(rhs);
-            return result;
-        } catch (CloneNotSupportedException e) {
-            return null;
-        }
+        return null;
     }
 
     void mul(Scalar rhs);
 
     static Scalar div(Scalar lhs, Scalar rhs) {
-        try {
-            Scalar result = (Scalar)lhs.clone();
-            result.div(rhs);
-            return result;
-        } catch (CloneNotSupportedException e) {
-            return null;
-        }
+        return null;
     }
 
     void div(Scalar rhs);
