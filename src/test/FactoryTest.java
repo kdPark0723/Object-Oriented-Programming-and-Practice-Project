8eff0e5f7ff4c2d741e21353679a58e953ad0f48
박시율 <kdpark0723@gmail.com>
2019-06-07 08:55:54 +0900

    .

--- src/test/FactoryTest.java
+++ src/test/FactoryTest.java
@@ -1,6 +1,8 @@
 package test;
 
-import tensor.*;
+import tensor.Factory;
+import tensor.Scalar;
+import tensor.WrongCSVFormatException;
 
 class FactoryTest extends BaseTest {
     FactoryTest() {
@@ -13,10 +15,10 @@ class FactoryTest extends BaseTest {
         BaseTester test = addTest(name);
 
         test.addTest("값(double 혹은 Double)을 지정하여 스칼라를 생성할 수 있다", () -> {
-            Scalar scalarCreateByInitValue = Factory.createScalarFromInitialValue(1.0);
+            Factory.createScalarFromInitialValue(1.0);
             test.assertTrue(true, "예외 없이 스칼라가 생성됨(double)");
 
-            scalarCreateByInitValue = Factory.createScalarFromInitialValue(new Double(1.0));
+            Factory.createScalarFromInitialValue(new Double(1.0));
             test.assertTrue(true, "예외 없이 스칼라가 생성됨(Double)");
         });
 
@@ -30,18 +32,18 @@ class FactoryTest extends BaseTest {
         BaseTester test = addTest(name);
 
         test.addTest("지정한 하나의 값을 모든 요소의 값으로 하는 n-차원 벡터를 생성할 수 있다", () -> {
-            Vector vectorCreateByInitValue = Factory.createVectorFromInitialValue(10, Factory.createScalarFromInitialValue(1.0));
+            Factory.createVectorFromInitialValue(10, Factory.createScalarFromInitialValue(1.0));
             test.assertTrue(true, "예외 없이 벡터가 생성됨");
         });
 
         test.addTest("i 이상 j 미만의 무작위 값을 요소로 하는 n-차원 벡터를 생성할 수 있다", () -> {
-            Vector vectorCreateByMinMaxValue = Factory.createVectorFromRandomValue(10, Factory.createScalarFromInitialValue(1.0), Factory.createScalarFromInitialValue(100.0));
+            Factory.createVectorFromRandomValue(10, Factory.createScalarFromInitialValue(1.0), Factory.createScalarFromInitialValue(100.0));
             test.assertTrue(true, "예외 없이 벡터가 생성됨");
         });
 
         test.addTest("1차원 배열로부터 n-차원 벡터를 생성할 수 있다", () -> {
             Scalar[] arr = new Scalar[10];
-            Vector vectorCreateByArray = Factory.createVectorFromArray(arr);
+            Factory.createVectorFromArray(arr);
             test.assertTrue(true, "예외 없이 벡터가 생성됨");
         });
     }
@@ -50,17 +52,17 @@ class FactoryTest extends BaseTest {
         BaseTester test = addTest(name);
 
         test.addTest("지정한 하나의 값을 모든 요소의 값으로 하는 m x n 행렬을 생성할 수 있다", () -> {
-            Matrix matrixCreatedByInitValue = Factory.createMatrixFromInitialValue(10, 10, Factory.createScalarFromInitialValue(1.0));
+            Factory.createMatrixFromInitialValue(10, 10, Factory.createScalarFromInitialValue(1.0));
             test.assertTrue(true, "예외 없이 행렬이 생성됨");
         });
 
         test.addTest("i 이상 j 미만의 무작위 값을 요소로 하는 m x n 행렬을 생성할 수 있다", () -> {
-            Matrix matrixCreatedByMinMaxValue = Factory.createMatrixFromRandomValue(10, 10, Factory.createScalarFromInitialValue(1.0), Factory.createScalarFromInitialValue(100.0));
+            Factory.createMatrixFromRandomValue(10, 10, Factory.createScalarFromInitialValue(1.0), Factory.createScalarFromInitialValue(100.0));
             test.assertTrue(true, "예외 없이 행렬이 생성됨");
         });
 
         test.addTest("csv 파일로부터 m x n 행렬을 생성할 수 있다", () -> {
-            Matrix matrixCreatedByFile = Factory.createMatrixFromCSVFile("src/test/passDummyMatrix.csv");
+            Factory.createMatrixFromCSVFile("src/test/passDummyMatrix.csv");
             test.assertTrue(true, "예외 없이 행렬이 생성됨");
 
             try {
@@ -72,12 +74,12 @@ class FactoryTest extends BaseTest {
         });
 
         test.addTest("2차원 배열로부터 m x n 행렬을 생성할 수 있다", () -> {
-            Matrix matrixCreatedByArray = Factory.createMatrixFromArray(new Scalar[10][10]);
+            Factory.createMatrixFromArray(new Scalar[10][10]);
             test.assertTrue(true, "예외 없이 행렬이 생성됨");
         });
 
         test.addTest("단위 행렬을 생성할 수 있다", () -> {
-            Matrix matrixCreatedByUnit = Factory.createIdentityMatrix(10);
+            Factory.createIdentityMatrix(10);
             test.assertTrue(true, "예외 없이 행렬이 생성됨");
         });
     }
